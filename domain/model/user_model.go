dc492ea5f3af9b80cb45afdff499517e1d5e9578
umardev500 <umar.dev500@gmail.com>
2023-10-07 22:35:07 +0700

    update user model

--- domain/model/user_model.go
+++ domain/model/user_model.go
@@ -8,12 +8,12 @@ type UserDetailModel struct {
 }
 
 type UserModel struct {
-	Username  string          `json:"username"`
-	Password  string          `json:"password"`
-	Status    string          `json:"status"`
-	Level     string          `json:"level"`
-	Detail    UserDetailModel `json:"detail"`
-	CreatedAt int64           `json:"created_at"`
-	UpdatedAt int64           `json:"updated_at"`
-	DeletedAt int64           `json:"deleted_at"`
+	Username  string          `json:"username" bson:"username"`
+	Password  string          `json:"password" bson:"password"`
+	Status    string          `json:"status" bson:"status"`
+	Level     string          `json:"level" bson:"level"`
+	Detail    UserDetailModel `json:"detail" bson:"detail"`
+	CreatedAt int64           `json:"created_at" bson:"created_at"`
+	UpdatedAt int64           `json:"updated_at" bson:"updated_at"`
+	DeletedAt int64           `json:"deleted_at" bson:"deleted_at"`
 }
