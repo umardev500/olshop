42ea0dc2261c3081ebbf1f47197101acb279d895
umardev500 <umar.dev500@gmail.com>
2023-10-07 22:23:17 +0700

    get user by id initial

--- application/controller/auth/login.go
+++ application/controller/auth/login.go
@@ -1,8 +1,25 @@
 package auth
 
-import fiber "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"olshop/domain/model"
+	"time"
+
+	fiber "github.com/gofiber/fiber/v2"
+)
 
 // Login implements domain.AuthController.
-func (*authController) Login(c *fiber.Ctx) error {
-	return c.JSON("ok")
+func (a *authController) Login(c *fiber.Ctx) error {
+	var creds model.LoginRequest
+	if err := c.BodyParser(&creds); err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(c.Context(), 5*time.Second)
+	defer cancel()
+	userData, err := a.userUC.FindByUsername(ctx, creds.Username)
+	if err != nil {
+		return err
+	}
+	return c.JSON(userData)
 }
