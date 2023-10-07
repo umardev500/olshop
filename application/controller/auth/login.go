72dca0a4f6c0d546316aee8a6363df7df58f4b5e
umardev500 <umar.dev500@gmail.com>
2023-10-07 22:31:15 +0700

    add validation to login handler

--- application/controller/auth/login.go
+++ application/controller/auth/login.go
@@ -2,7 +2,9 @@ package auth
 
 import (
 	"context"
+	"olshop/application/controller"
 	"olshop/domain/model"
+	"olshop/helper"
 	"time"
 
 	fiber "github.com/gofiber/fiber/v2"
@@ -12,7 +14,13 @@ import (
 func (a *authController) Login(c *fiber.Ctx) error {
 	var creds model.LoginRequest
 	if err := c.BodyParser(&creds); err != nil {
-		return err
+		return controller.FiberErr(c, fiber.StatusBadRequest, err.Error(), nil)
+	}
+
+	// validate
+	detail, err := helper.ValidateStruct(a.validate, creds)
+	if err != nil {
+		return controller.FiberErr(c, fiber.StatusUnprocessableEntity, "validation error", detail)
 	}
 
 	ctx, cancel := context.WithTimeout(c.Context(), 5*time.Second)
